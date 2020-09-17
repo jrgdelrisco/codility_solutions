79a46ef57ca5ffd65126c8b8858326bc48d247a7
jrgdelrisco <jrgdelrisco@gmail.com>
2020-09-17 20:44:24 +0200

    Lesson 1 Binary Gap

--- codility_solutions.py
+++ codility_solutions.py
@@ -1,5 +1,15 @@
+# lesson 1 binary gap
+def solution(N):
+    n = str(bin(N))[2:]
+    indexes_one = [i for i in range(len(n)) if n[i] == '1']
+   
+    if len(indexes_one) <= 1:
+        return 0
+   
+    return max([indexes_one[i] - indexes_one[i - 1] for i in range(1, len(indexes_one))]) - 1
+
 # lesson 2 cyclic rotation
-def solution(A, K):
-    k = K % len(A)
-    a = A[len(A) - k:] + A[:len(A) - k]
-    return a
+# def solution(A, K):
+#     k = K % len(A)
+#     a = A[len(A) - k:] + A[:len(A) - k]
+#     return a
