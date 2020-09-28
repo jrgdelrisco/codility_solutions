b0406a41da2b18917b52c114780c85f11c8cf844
jrgdelrisco <jrgdelrisco@gmail.com>
2020-09-28 23:26:23 +0200

    lesson 3 tape equilibrium

--- codility_solutions.py
+++ codility_solutions.py
@@ -34,3 +34,17 @@ def solution(A):
     for a in A:
         found[a - 1] = a
     return found.index(0) + 1
+
+#lesson 3 tape equilibrium
+def solution(A):
+    if not A:
+        return 0
+    from itertools import  accumulate
+    sumsFoward = list(accumulate(A))
+    sumsBackward = list(accumulate(A[::-1]))[::-1]
+    minimum = float('inf')
+    for i in range(len(A) - 1):
+        tmp = abs(sumsFoward[i] - sumsBackward[i + 1])
+        if minimum > tmp:
+            minimum = tmp    
+    return minimum
