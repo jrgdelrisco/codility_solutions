63c1fbb294428b3a199b79c4693679f9b752739f
jrgdelrisco <jrgdelrisco@gmail.com>
2020-09-29 23:11:22 +0200

    lesson 4 Frog River One

--- codility_solutions.py
+++ codility_solutions.py
@@ -29,22 +29,35 @@
 #     return (Y - X)//D + ((Y - X) % D != 0)
 
 # lesson 3 find the missing element in a given permutation
-def solution(A):
-    found = [0] * (len(A) + 1)
-    for a in A:
-        found[a - 1] = a
-    return found.index(0) + 1
+# def solution(A):
+    # found = [0] * (len(A) + 1)
+    # for a in A:
+    #     found[a - 1] = a
+    # return found.index(0) + 1
 
 #lesson 3 tape equilibrium
-def solution(A):
-    if not A:
-        return 0
-    from itertools import  accumulate
-    sumsFoward = list(accumulate(A))
-    sumsBackward = list(accumulate(A[::-1]))[::-1]
-    minimum = float('inf')
-    for i in range(len(A) - 1):
-        tmp = abs(sumsFoward[i] - sumsBackward[i + 1])
-        if minimum > tmp:
-            minimum = tmp    
-    return minimum
+# def solution(A):
+#     if not A:
+#         return 0
+#     from itertools import  accumulate
+#     sumsFoward = list(accumulate(A))
+#     sumsBackward = list(accumulate(A[::-1]))[::-1]
+#     minimum = float('inf')
+#     for i in range(len(A) - 1):
+#         tmp = abs(sumsFoward[i] - sumsBackward[i + 1])
+#         if minimum > tmp:
+#             minimum = tmp    
+#     return minimum
+
+#lesson 4  frog river one
+def solution(X, A):
+    positions_set = set(A)
+    valid_pos_count = 0
+    for second, a in enumerate(A):
+        if a <= X and a in positions_set:
+            positions_set.remove(a)
+            valid_pos_count+=1
+        if valid_pos_count == X:
+            return second
+    return -1
+        
